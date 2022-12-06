2c6c79c2f541b8fcdb6ec6b934db6773f2467aad
mgcodeur <mgcodeur@gmail.com>
2022-12-06 17:08:48 +0300

    api register l5-swagger documentation

--- src/Http/Controllers/Api/V1/Auth/RegisterController.php
+++ src/Http/Controllers/Api/V1/Auth/RegisterController.php
@@ -8,6 +8,60 @@ use Mgcodeur\LaravelSanctum\Http\Resources\Api\Auth\LoginResource;
 
 class RegisterController
 {
+    /**
+     * @OA\Post(
+     *      path="/api/v1/auth/register",
+     *      tags={"Auth"},
+     *      summary="Api Register (Pour inscrire l'utilisateur au plateforme)",
+     *      description="Retourne le token d'accès (à utiliser comme un bearer token)",
+     *      @OA\Parameter(
+     *         name="first_name",
+     *         in="query",
+     *         description="Votre nom (Ex: John)",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *      ),
+     *      @OA\Parameter(
+     *         name="last_name",
+     *         in="query",
+     *         description="Votre prenom (Ex: Doe)",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *      ),
+     *      @OA\Parameter(
+     *         name="email",
+     *         in="query",
+     *         description="Votre adresse email (Ex: mgcodeur@gmail.com)",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *      ),
+     *      @OA\Parameter(
+     *         name="password",
+     *         in="query",
+     *         description="Votre mot de passe (Ex: password)",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent()
+     *      ),
+     *       @OA\Response(
+     *          response=400,
+     *          description="Bad Request",
+     *          @OA\JsonContent()
+     *       )
+     * )
+     **/
     public function __invoke(RegisterRequest $request)
     {
         $user = LaravelSanctum::getAuthModel()::create($request->all());
